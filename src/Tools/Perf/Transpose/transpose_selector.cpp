ff7af74192a89c033b3e0974fcc92cae9dc90b32
Gerhard Baer <Gerhard.Baer@protonmail.com>
2023-12-16 18:17:44 +0100

    Revert "transpose_selector: gracefully dispatch to all supported variants."
    This reverts commit 22e607bc4913f0a29ecbf5147c046d205d521167.

--- src/Tools/Perf/Transpose/transpose_selector.cpp
+++ src/Tools/Perf/Transpose/transpose_selector.cpp
@@ -2,8 +2,9 @@
 #include <cstdint>
 
 #include "Tools/Exception/exception.hpp"
-#if defined(__i386__) || defined(__x86_64__)
+#ifdef __AVX2__
 #include "Tools/Perf/Transpose/transpose_AVX.h"
+#elif defined(__SSE4_1__)
 #include "Tools/Perf/Transpose/transpose_SSE.h"
 #endif
 #if defined(__ARM_NEON__) || defined(__ARM_NEON)
@@ -13,80 +14,78 @@
 
 bool aff3ct::tools::char_transpose(const signed char *src, signed char *dst, int n)
 {
-#if defined(__AVX2__)
-	do {
-		const int min_n = 256;
-		if (n < min_n)
-			break;
+#if defined(__MIC__) || defined(__KNCNI__) || defined(__AVX512__) || defined(__AVX512F__)
+	int min_n = std::numeric_limits<int>::max();
+#elif defined(__AVX2__)
+	int min_n = 256;
+#elif defined(__SSE4_1__) || (defined(__ARM_NEON__) || defined(__ARM_NEON))
+	int min_n = 128;
+#else
+	int min_n = std::numeric_limits<int>::max();
+#endif
+
+	if (n >= min_n)
+	{
+#if defined(__MIC__) || defined(__KNCNI__) || defined(__AVX512__) || defined(__AVX512F__)
+		return false;
+#elif defined(__AVX2__)
 		if (((uintptr_t)src) % (min_n / 8))
-			break;
+			throw runtime_error(__FILE__, __LINE__, __func__, "'src' is unaligned memory.");
 		if (((uintptr_t)dst) % (min_n / 8))
-			break;
+			throw runtime_error(__FILE__, __LINE__, __func__, "'dst' is unaligned memory.");
 		uchar_transpose_avx((__m256i*) src, (__m256i*) dst, n);
 		return true;
-	} while (0);
-#endif
-#if defined(__SSE4_1__)
-	do {
-		const int min_n = 128;
-		if (n < min_n)
-			break;
+#elif defined(__SSE4_1__)
 		if (((uintptr_t)src) % (min_n / 8))
-			break;
+			throw runtime_error(__FILE__, __LINE__, __func__, "'src' is unaligned memory.");
 		if (((uintptr_t)dst) % (min_n / 8))
-			break;
+			throw runtime_error(__FILE__, __LINE__, __func__, "'dst' is unaligned memory.");
 		uchar_transpose_sse((__m128i*) src, (__m128i*) dst, n);
 		return true;
-	} while (0);
-#endif
-#if (defined(__ARM_NEON__) || defined(__ARM_NEON))
-	do {
-		const int min_n = 128;
-		if (n < min_n)
-			break;
+#elif (defined(__ARM_NEON__) || defined(__ARM_NEON))
 		uchar_transpose_neon((trans_TYPE*) src, (trans_TYPE*) dst, n);
 		return true;
-	} while (0);
 #endif
+	}
+
 	return false;
 }
 
 bool aff3ct::tools::char_itranspose(const signed char *src, signed char *dst, int n)
 {
-#if defined(__AVX2__)
-	do {
-		const int min_n = 256;
-		if (n < min_n)
-			break;
+#if defined(__MIC__) || defined(__KNCNI__) || defined(__AVX512__) || defined(__AVX512F__)
+	int min_n = std::numeric_limits<int>::max();
+#elif defined(__AVX2__)
+	int min_n = 256;
+#elif defined(__SSE4_1__) || (defined(__ARM_NEON__) || defined(__ARM_NEON))
+	int min_n = 128;
+#else
+	int min_n = std::numeric_limits<int>::max();
+#endif
+
+	if (n >= min_n)
+	{
+#if defined(__MIC__) || defined(__KNCNI__) || defined(__AVX512__) || defined(__AVX512F__)
+		return false;
+#elif defined(__AVX2__)
 		if (((uintptr_t)src) % (min_n / 8))
-			break;
+			throw runtime_error(__FILE__, __LINE__, __func__, "'src' is unaligned memory.");
 		if (((uintptr_t)dst) % (min_n / 8))
-			break;
+			throw runtime_error(__FILE__, __LINE__, __func__, "'dst' is unaligned memory.");
 		uchar_itranspose_avx((__m256i*) src, (__m256i*) dst, n / 8);
 		return true;
-	} while (0);
-#endif
-#if defined(__SSE4_1__)
-	do {
-		const int min_n = 128;
-		if (n < min_n)
-			break;
+#elif defined(__SSE4_1__)
 		if (((uintptr_t)src) % (min_n / 8))
-			break;
+			throw runtime_error(__FILE__, __LINE__, __func__, "'src' is unaligned memory.");
 		if (((uintptr_t)dst) % (min_n / 8))
-			break;
+			throw runtime_error(__FILE__, __LINE__, __func__, "'dst' is unaligned memory.");
 		uchar_itranspose_sse((__m128i*) src, (__m128i*) dst, n / 8);
 		return true;
-	} while (0);
-#endif
-#if (defined(__ARM_NEON__) || defined(__ARM_NEON))
-	do {
-		const int min_n = 128;
-		if (n < min_n)
-			break;
+#elif (defined(__ARM_NEON__) || defined(__ARM_NEON))
 		uchar_itranspose_neon((trans_TYPE*) src, (trans_TYPE*) dst, n / 8);
 		return true;
-	} while (0);
 #endif
+	}
+
 	return false;
 }
